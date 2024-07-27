1edd2228237859643eb178c8d5571592d94accf1
sacheex <sachithdhanushka00@gmail.com>
2024-07-27 22:29:31 +0530

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { Stack, Typography } from "@mui/material"
+
+export const Home = () => {
+    return(
+        <>
+        <Stack align={'center'} > 
+        <Typography variant='h2' mt={10}>
+            Eventor
+        </Typography>
+        <Typography variant="body1">An Event Managment System</Typography>
+
+        </Stack>
+        </>
+    )
+}
