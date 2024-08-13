7ffb4707a57b0b7de69b4e91fd6c09d5c1a5ffcc
sacheex <sachithdhanushka00@gmail.com>
2024-08-13 18:51:46 +0530

    Add path to addEvent and adminDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { Home } from "./components/Home"
-import { LoginPage } from "./components/LoginPage"
+import { Login } from "./components/Login"
 import { NavBar } from "./components/NavBar"
 import { Route, Routes } from "react-router-dom"
+import { Register } from "./components/Register"
+import { AddEvent } from "./components/AddEvent"
+import { AdminDashboard } from "./components/AdminDashboard"
 
 function App() {
 
   return (
-    <>
+   <>
     <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="add-event" element={<AddEvent />} />
+        <Route path="admin-dashboard" element={<AdminDashboard />} />
       </Routes>
-    </>
+   </>
+
   )
 }
 
