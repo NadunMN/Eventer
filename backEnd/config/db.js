20305cbbc14c7ffb1be8d5bc0246d2dd17cc5cce
sacheex <sachithdhanushka00@gmail.com>
2024-08-23 03:47:09 +0530

    remove deprecated options

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // Connect to MongoDB
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected!")   
     } catch (error) {
         console.log("MongoDB connection error: ", error)
