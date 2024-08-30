a6bf9df8f751b373807a8cf4e8198edeb1512a4e
sacheex <sachithdhanushka00@gmail.com>
2024-08-31 03:12:32 +0530

    change user routes

--- backEnd/routes/userRoutes.js
+++ backEnd/routes/userRoutes.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const { getAllUser, getUser, register } = require("../controllers/userController")
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUser,
+  getUser,
+  register,
+} = require("../controllers/userController");
 
-router.get('/user', getAllUser)
-router.get('/user/:id', getUser)
-router.post('/register', register)
+router.get("/users", getAllUser);
+router.get("/user/:id", getUser);
+router.post("/register", register);
 
 module.exports = router;
