89bbb7882e2aeb795ace606b3fab56981cc50c58
sacheex <sachithdhanushka00@gmail.com>
2024-08-31 03:12:04 +0530

    change user schema

--- backEnd/models/userModel.js
+++ backEnd/models/userModel.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
-  fName: { type: String, required: true },
-  lName: { type: String, required: true },
-  role: { type: String, required: true},
-  email: { type: String, required: true, unique: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String },
   username: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone_number: { type: String },
   password: { type: String, required: true },
+  created_at: { type: String, required: true },
+  updated_at: { type: String },
+  profile_picture_url: { type: String },
 });
 
 // // Create the user model
