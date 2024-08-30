b6cfd3b59f1b7fe046f8a7cf06646882eeb8cb87
sacheex <sachithdhanushka00@gmail.com>
2024-08-31 03:13:21 +0530

    change event schema

--- backEnd/models/eventModel.js
+++ backEnd/models/eventModel.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 // Define the event schema
 const EventSchema = new mongoose.Schema({
-  title: {type: String, required: true},
-  start_date: {type: String, required: true},
-  start_time: {type: String, required: true},
-  end_date: {type: String},
-  end_time: {type: String},
-  description: {type: String},
-  venue: {type: String, required: true},
-  capacity: {type: Number},
-  participants: {type: String},
-  image: {type: String, required: true},
-})
+  title: { type: String, required: true },
+  start_date: { type: String, required: true },
+  start_time: { type: String, required: true },
+  end_date: { type: String },
+  end_time: { type: String },
+  description: { type: String },
+  venue: { type: String, required: true },
+  capacity: { type: Number },
+  participants: { type: String },
+  image: { type: String, required: true },
+});
 
 // // Create the event model
-const EventModel = mongoose.model("event", EventSchema);
+const EventModel = mongoose.model("events", EventSchema);
 
 module.exports = EventModel;
