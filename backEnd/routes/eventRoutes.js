ab88f49f8c4d850ab0694fdde60e8d44f4b230b4
dinuka2001 <dinukasahan2001@gmail.com>
2024-09-01 10:26:02 +0530

    add find event byid endpoint

--- backEnd/routes/eventRoutes.js
+++ backEnd/routes/eventRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getEvents, searchEvents, createEvent } = require("../controllers/eventController")
+const { getEvents, searchEvents, createEvent, getOneEvent } = require("../controllers/eventController")
 
 router.get('/getEvent', getEvents)
 router.get('/searchEvents', searchEvents)
 router.post('/createEvent', createEvent)
+// router.get('/getOne', getOneEvent)
+router.get('/getEvent/:id', getOneEvent)
 
 module.exports = router
