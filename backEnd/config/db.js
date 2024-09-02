e52b30a34ef3f046d7f14918568f25bcb7e3f7b2
dinuka2001 <dinukasahan2001@gmail.com>
2024-09-02 11:12:16 +0530

    create getOneEvent end point , set dynamic route in routes

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // Connect to MongoDB
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/event");
         console.log("MongoDB connected!")   
     } catch (error) {
         console.log("MongoDB connection error: ", error)
