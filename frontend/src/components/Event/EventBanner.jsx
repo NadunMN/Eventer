e2a244c75c0bc30635581221a1febc8e3a61e3d5
sacheex <sachithdhanushka00@gmail.com>
2024-09-05 04:08:04 +0530

    add  favourite event feature

--- frontend/src/components/Event/EventBanner.jsx
+++ frontend/src/components/Event/EventBanner.jsx
@@ -1,8 +1,54 @@
 import { Card, CardMedia, Box, IconButton, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 function EventBanner({ event }) {
-  console.log(event.cover_image);
+  const [isFav, setIsFev] = useState(false);
+
+  const [userRole, setUserRole] = useState("");
+  const [userId, setUserId] = useState("");
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      const jwtToken = jwtDecode(user.token);
+      setUserId(jwtToken._id);
+      setUserRole(jwtToken.role);
+      axios
+        .get(`http://localhost:5000/api/user/${jwtToken._id}`)
+        .then((res) => {
+          setFavorites(res.data.favourite_events || []);
+        })
+        .catch((err) => {
+          console.error("Failed to fetch user data", err);
+        });
+    }
+  }, []);
+  const handleFav = (event_id) => {
+    console.log(event_id);
+
+    const isFav = favorites.includes(event_id);
+    const updatedFavorites = isFav
+      ? favorites.filter((id) => id !== event_id)
+      : [...favorites, event_id];
+
+    axios
+      .put(`http://localhost:5000/api/user/edit/${userId}`, {
+        favourite_events: updatedFavorites,
+      })
+      .then(() => {
+        setFavorites(updatedFavorites);
+        console.log(isFav ? "Removed from favorites" : "Added to favorites");
+      })
+      .catch((err) => {
+        alert("An error occurred. Please check the console");
+        console.error(err);
+      });
+  };
   return (
     <Box>
       <Box
@@ -19,11 +65,16 @@ function EventBanner({ event }) {
         }}
       >
         <IconButton
-          sx={{ position: "absolute", bottom: 10, right: 40, color: "gray" }}
-          aria-label="add to favorites"
-          size="xl"
+          variant="outlined"
+          color={favorites.includes(event._id) ? "warning" : "neutral"}
+          sx={{ position: "absolute", bottom: 10, right: 40 }}
+          onClick={() => handleFav(event._id)}
         >
-          <FavoriteBorderIcon />
+          {favorites.includes(event._id) ? (
+            <FavoriteIcon sx={{ fontSize: 30 }} />
+          ) : (
+            <FavoriteBorderIcon sx={{ fontSize: 30 }} />
+          )}
         </IconButton>
       </Box>
       <Box>
